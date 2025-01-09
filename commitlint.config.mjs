b57edbb753d9a02581dde1366dba649ca4aeb21b
Viibrant <11376396+Viibrant@users.noreply.github.com>
2025-01-09 03:41:08 +0000

    chore: remove commitlint rules for now

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,7 +2,5 @@ export default {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'ci', 'perf', 'chore', 'revert']],
-        'scope-empty': [2, 'never'],
-        'subject-case': [2, 'always', ['lower-case']],
     },
 };

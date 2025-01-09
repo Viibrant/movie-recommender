fe223382d198145c849143b30ce7aca05dfb24ba
Viibrant <11376396+Viibrant@users.noreply.github.com>
2025-01-09 03:45:41 +0000

    chore(ci): disable rules

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,5 +2,7 @@ export default {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'ci', 'perf', 'chore', 'revert']],
-    },
+        // Disable
+        'scope-empty': [0, 'never'],
+        'subject-case': [0, 'always'],
 };

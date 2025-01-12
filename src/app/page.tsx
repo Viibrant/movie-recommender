a80f9a9e3ce88798fe7a5cf865c510804193374f
zzacharyv <zzacharyv@gmail.com>
2025-01-11 17:45:19 -0700

    genre buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,33 +77,54 @@ export default function Home() {
           {/* Genre Buttons */}
         </div>
         <div className="flex justify-center mb-8">
-          <button type="button" // Add the type attribute
+          <button
+            type="button" // Add the type attribute
             onClick={() => { fetchMoviesByGenre(28).then(setMovies) }}
-            className="ml-4 px-6 py-2 bg-yellow-600 hover:bg-yellow-700 rounded-md text-white">Action
+            className="ml-4 px-6 py-2 bg-yellow-600 hover:bg-yellow-700 rounded-md text-white"
+          >
+            Action
           </button>
-          <button type="button" // Add the type attribute
+          <button
+            type="button" // Add the type attribute
             onClick={() => { fetchMoviesByGenre(12).then(setMovies) }}
-            className="ml-4 px-6 py-2 bg-yellow-600 hover:bg-yellow-700 rounded-md text-white">Adventure
+            className="ml-4 px-6 py-2 bg-yellow-600 hover:bg-yellow-700 rounded-md text-white"
+          >
+            Adventure
           </button>
-          <button type="button" // Add the type attribute
+          <button
+            type="button" // Add the type attribute
             onClick={() => { fetchMoviesByGenre(35).then(setMovies) }}
-            className="ml-4 px-6 py-2 bg-yellow-600 hover:bg-yellow-700 rounded-md text-white">Comedy
+            className="ml-4 px-6 py-2 bg-yellow-600 hover:bg-yellow-700 rounded-md text-white"
+          >
+            Comedy
           </button>
-          <button type="button" // Add the type attribute
+          <button
+            type="button" // Add the type attribute
             onClick={() => { fetchMoviesByGenre(18).then(setMovies) }}
-            className="ml-4 px-6 py-2 bg-yellow-600 hover:bg-yellow-700 rounded-md text-white">Drama
+            className="ml-4 px-6 py-2 bg-yellow-600 hover:bg-yellow-700 rounded-md text-white"
+          >
+            Drama
           </button>
-          <button type="button" // Add the type attribute
+          <button
+            type="button" // Add the type attribute
             onClick={() => { fetchMoviesByGenre(27).then(setMovies) }}
-            className="ml-4 px-6 py-2 bg-yellow-600 hover:bg-yellow-700 rounded-md text-white">Horror
+            className="ml-4 px-6 py-2 bg-yellow-600 hover:bg-yellow-700 rounded-md text-white"
+          >
+            Horror
           </button>
-          <button type="button" // Add the type attribute
+          <button
+            type="button" // Add the type attribute
             onClick={() => { fetchMoviesByGenre(10749).then(setMovies) }}
-            className="ml-4 px-6 py-2 bg-yellow-600 hover:bg-yellow-700 rounded-md text-white">Romance
+            className="ml-4 px-6 py-2 bg-yellow-600 hover:bg-yellow-700 rounded-md text-white"
+          >
+            Romance
           </button>
-          <button type="button" // Add the type attribute
+          <button
+            type="button" // Add the type attribute
             onClick={() => { fetchMoviesByGenre(53).then(setMovies) }}
-            className="ml-4 px-6 py-2 bg-yellow-600 hover:bg-yellow-700 rounded-md text-white">Thriller
+            className="ml-4 px-6 py-2 bg-yellow-600 hover:bg-yellow-700 rounded-md text-white"
+          >
+            Thriller
           </button>
         </div>
 
